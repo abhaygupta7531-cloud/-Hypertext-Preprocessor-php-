e7a7dc91f0c316e8df3c2297130ad62bf373f12a
Abhay Gupta <abhaygupta7531@gmail.com>
2026-05-28 20:21:21 +0530

    armstrong number

--- abhay6.php
+++ abhay6.php
@@ -3,9 +3,10 @@ $num=153;
 $original=$num;
 $sum=0;
 
-while($num>10){
+while($num>0){
     $remender=$num%10;
     $sum=$sum+($remender*$remender*$remender);
+    $num=(int)($num/10);
 }if($original==$sum){
     echo"original is an armstrong number.";
 }else{
