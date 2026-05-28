a551a021793d781d0ed9cebb40a3e85b7195f81a
Abhay Gupta <abhaygupta7531@gmail.com>
2026-05-28 19:18:44 +0530

    max and min  in  php

---
+++ abhay4.php
@@ -0,0 +1,13 @@
+<?php
+$name="abhay";
+$rollno="7335";
+$class="FYBCA";
+$marks="86";
+
+echo"<h2> Student information</h2>";
+echo"Name:".$name."<br>";
+echo"Rollno:".$rollno."<br>";
+echo"Class:".$class."<br>";
+echo"Marks:".$marks."<br>";
+
+?>
---
+++ abhay5.php
@@ -0,0 +1,18 @@
+<?php
+$a=12;
+$b=56;
+$c=45;
+
+$max=max($a,$b,$c);
+echo"Maximum number is:".$max;
+echo"<br>";
+?>
+
+<?php
+$a=12;
+$b=56;
+$c=45;
+
+$min=min($a,$b,$c);
+echo"Minimum number is:".$min;
+?>
