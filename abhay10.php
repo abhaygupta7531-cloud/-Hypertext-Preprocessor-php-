6c1281468dbda6aa58723da796c3d544d57efa1a
Abhay Gupta <abhaygupta7531@gmail.com>
2026-05-29 07:12:23 +0530

    xml 1

--- abhay10.php
+++ abhay10.php
@@ -1,3 +1,19 @@
 <?php
 $xml = new DOMDocument("1.0", "UTF-8");
+$college=$xml->createElement("Modern_college");
+$dept=$xml->createElement("Computer_application_Department");
 $xml->formatOutput = true;
+$xml->appendChild($college);
+$college->appendChild($dept);
+$course=$xml->createElement("Course","BCA");
+$student=$xml->createElement("Student_strength","80");
+$teacher=$xml->createElement("No_of_teacher","12");
+
+$dept->appendChild($course);
+$dept->appendChild($student);
+$dept->appendChild($teacher);
+$xml->save("College.xml");
+echo"xml File generated Successfully!";
+?>
+
+
